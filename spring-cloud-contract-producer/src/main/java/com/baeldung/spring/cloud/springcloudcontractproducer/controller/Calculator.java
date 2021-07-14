6004d3c3d5330b39d104d475d5552c296e45150a
chenjian <com.me@foxmail.com>
2021-07-14 17:40:06 +0800

    format

--- spring-cloud-contract-producer/src/main/java/com/baeldung/spring/cloud/springcloudcontractproducer/controller/Calculator.java
+++ spring-cloud-contract-producer/src/main/java/com/baeldung/spring/cloud/springcloudcontractproducer/controller/Calculator.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
+
     public boolean isNumberPrime(Integer number) {
         return number % 2 == 0;
     }
+
 }
--- spring-cloud-contract-producer/src/main/java/com/baeldung/spring/cloud/springcloudcontractproducer/controller/EvenOddController.java
+++ spring-cloud-contract-producer/src/main/java/com/baeldung/spring/cloud/springcloudcontractproducer/controller/EvenOddController.java
@@ -1,6 +1,5 @@
 package com.baeldung.spring.cloud.springcloudcontractproducer.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
